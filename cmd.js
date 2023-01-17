9bf026bf6354dfd9be499d9d23d10067cc27687f
davidjhaa4u <davidjhaa4u@gmail.com>
2023-01-17 15:58:16 +0530

    taking input from cli implemented

--- cmd.js
+++ cmd.js
@@ -1,1 +1,10 @@
-let 
+//  FS module allows you to work with the file system on your computer
+let fs = require("fs");
+// process module is useful in taking command line argument         
+//also it is preinstalled in nodejs so we do not need to require it
+let process = require("process");
+
+// also process.argv returns all the argument from cli as an array
+let inputarr = process.argv.slice(2);
+
+console.log(inputarr);
