de0bac8e99e0830368f3588f0bae7c3ed529c1cc
davidjhaa4u <davidjhaa4u@gmail.com>
2023-01-18 15:25:00 +0530

    code refactored

--- cmd.js
+++ cmd.js
@@ -1,8 +1,8 @@
 //  FS module allows you to work with the file system on your computer
-let fs = require("fs");
+const fs = require("fs");
 // process module is useful in taking command line argument         
 //also it is preinstalled in nodejs so we do not need to require it
-let process = require("process");
+const process = require("process");
 
 // also process.argv returns all the argument from cli as an array
 let inputarray = process.argv.slice(2);
@@ -24,25 +24,32 @@ for(let i = 0; i < inputarray.length; i++){
 // console.log(optionsarray);
 // console.table(filesarray);
 
-let content = "";
-
-// for(let i = 0; i < filesarray.length; i++){
-    // content += filesarray[i] + "\n";
-// }
-// 
-// console.log("files to be read are \n" + content);
-
-// checking if the file exist or not
-
 for(let i = 0; i < filesarray.length; i++){
     let doesexist = fs.existsSync(filesarray[i]);
-    if(doesexist == false)
-        console.log("one or more files does not exist");
+    if(doesexist == false){
+        console.log(filesarray[i] + " :-> File/Files does not exist");
         return;
+    }
 }
+
+let files = "";
+for(let i = 0; i < filesarray.length; i++){
+    files += filesarray[i] + "\n";
+}
+
+console.log("Files to be read are :-> \n" + files);
+
+let content = "";
 
 for(let i = 0; i < filesarray.length; i++){
-    content += filesarray[i] + "\n";
+    content += fs.readFileSync(filesarray[i]) + "\n";
 }
 
-console.log("files to be read are \n" + content);
+console.log(content);
+
+let contentarray = content.split("\n");
+
+if(optionsarray.includes("-s"){
+
+}
+
